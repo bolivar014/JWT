cdae1041e9f41865ef153fec39c4e0393dfe99b5
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-12-23 20:36:06 -0500

    generando token de sesión

---
+++ JWT/Constants/UserConstants.cs
@@ -0,0 +1,14 @@
+﻿using JWT.Models;
+
+namespace JWT.Constants
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "pepitoperez", Password = "admin123", Rol = "Administrador", EmailAddress = "cliente@sincorreo.com", FirstName = "Pepito", LastName = "Perez" },
+            new UserModel() { Username = "pepito.perez", Password = "admin123", Rol = "Vendedor", EmailAddress = "cliente1@sincorreo.com", FirstName = "Pepito JR", LastName = "Perez L" },
+            new UserModel() { Username = "pepitoperez", Password = "admin123", Rol = "Administrador", EmailAddress = "cliente3@sincorreo.com", FirstName = "Pepito Alt", LastName = "Perez A" },
+        };
+    }
+}
--- JWT/Controllers/LoginController.cs
+++ JWT/Controllers/LoginController.cs
@@ -1,6 +1,12 @@
-﻿using JWT.Models;
+﻿using JWT.Constants;
+using JWT.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
 
 namespace JWT.Controllers
 {
@@ -8,10 +14,74 @@ namespace JWT.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+
+            _config = config;
+
+        }
         [HttpPost]
         public IActionResult Login(LoginUser userLogin)
         {
-            return Ok("User Login");
+            var user = Authenticate(userLogin);
+
+            if(user != null)
+            {
+                // Crear token
+                // return Ok("Usuario autenticado");
+                var token = Generate(user);
+
+                // Retornamos token
+                return Ok(token);
+            }
+            
+            // En caso que no exista
+            return NotFound("Usuario no encontrado");
+        }
+
+        private UserModel Authenticate(LoginUser userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(user => user.Username.ToLower() == userLogin.Username 
+            && user.Password == userLogin.Password);
+
+            // Retorno usuario
+            if(currentUser != null)
+            {
+                return currentUser;
+            }
+
+            // En caso que no exista
+            return null;
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Crear los claims
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim(ClaimTypes.Role, user.Rol),
+                new Claim("IdEmpresa", "141")
+            };
+
+            // Crear token
+            var token = new JwtSecurityToken(
+                _config["JWT:Issuer"],
+                _config["JWT:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToInt32(_config["JWT:MinutesDurationToken"])),
+                signingCredentials: credentials
+            );
+
+            // retornamos token
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
