8f552554498c8b25039ee021167c7cedd685f447
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2023-12-23 19:56:19 -0500

    integrando modelo user y loginController

---
+++ JWT/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using JWT.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login(LoginUser userLogin)
+        {
+            return Ok("User Login");
+        }
+    }
+}
